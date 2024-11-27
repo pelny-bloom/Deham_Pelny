d067683b9e8868eacad12fc135144d1e19f54bb3
pelny-bloom <pelny.peya@gmail.com>
2024-11-27 15:29:19 +0100

    create 2 public subnets

--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,26 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+# Create Public Subnet 1
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24" 
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-gallery-1"
+  }
+}
+
+# Create Public Subnet 1
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.2.0/28" 
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-gallery-2"
+  }
+}
