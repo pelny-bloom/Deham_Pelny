e45c5e798d971e36bb68634b590b0a884d4fdbf0
pelny-bloom <pelny.peya@gmail.com>
2024-11-27 16:02:24 +0100

    fixing cidr block for public RT

--- vpc.tf
+++ vpc.tf
@@ -45,7 +45,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.dev_vpc.id
 
   route {
-    cidr_block = var.cidr_blocks[0]
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
