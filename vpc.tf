b9572be22d7f8e51a299bfd225818d9cffb08cc2
pelny-bloom <pelny.peya@gmail.com>
2024-11-27 15:39:28 +0100

    fixing the cidr block for pub sub 1

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_internet_gateway" "igw" {
 # Create Public Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = "10.0.1.0/25" 
+  cidr_block              = "10.0.0.0/25" 
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
