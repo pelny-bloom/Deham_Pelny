3ab644c4027c8c5b328618c0345c6019cdc3b6f3
pelny-bloom <pelny.peya@gmail.com>
2024-11-27 15:13:10 +0100

    create test for vpc

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 # Create a VPC to launch our instances into
 resource "aws_vpc" "dev_vpc" {
-  cidr_block = "10.0.0.0/16"  
+  cidr_block = "10.0.0.0/24"  
   enable_dns_hostnames = true 
   enable_dns_support = true
   
   tags       =  {
-    Name     = "webserver vpc"
+    Name     = "gallery vpc"
   }       
 }
