dade61a9646d582a24a9a09c86f0ec20c3369691
pelny-bloom <pelny.peya@gmail.com>
2024-11-27 15:35:38 +0100

    create 2 subnets and fixing the cidr block

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_internet_gateway" "igw" {
 # Create Public Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = "10.0.1.0/24" 
+  cidr_block              = "10.0.1.0/25" 
   availability_zone       = "us-west-2a"
   map_public_ip_on_launch = true
 
@@ -32,7 +32,7 @@ resource "aws_subnet" "public_1" {
 # Create Public Subnet 1
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = "10.0.2.0/28" 
+  cidr_block              = "10.0.0.128/25" 
   availability_zone       = "us-west-2b"
   map_public_ip_on_launch = true
 
