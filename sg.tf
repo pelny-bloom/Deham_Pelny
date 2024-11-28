524f2c58788c851d09e341f2d0fe078b1c5d9a81
pelny-bloom <pelny.peya@gmail.com>
2024-11-28 11:54:41 +0100

    fixing cidr block for security groups for the ec2

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "gallery_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -17,7 +17,7 @@ resource "aws_security_group" "gallery_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
    ingress {
@@ -25,7 +25,7 @@ resource "aws_security_group" "gallery_sg" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -33,7 +33,7 @@ resource "aws_security_group" "gallery_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
